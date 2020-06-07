ef451b6647235fdbc9216415c51270a1f05c288f
DeltaNedas <deltanedas@gmail.com>
2020-06-07 03:13:47 +0100

    1.2.0: add buttons for console on mobile

--- scripts/button.js
+++ scripts/button.js
@@ -5,10 +5,24 @@ var shown = false;
 // button for poor mobile users
 ui.addButton("console", "terminal", () => {
 	shown = !shown;
-	// Prompt for command
-	if (shown) {
+});
+
+ui.addTable("bottom", "console", console => {
+	console.background(Tex.buttonTrans);
+	console.visible(boolp(() => shown));
+	console.defaults().margin(8);
+
+	// Clear the console
+	console.addImageButton(Icon.refresh, Styles.clearPartiali, 40, run(() => {
+		Vars.ui.scriptfrag.clearMessages();
+	}));
+
+	// Prompt for a line of JS
+	console.addImageButton(Icon.terminal, Styles.clearPartiali, 40, run(() => {
 		Vars.ui.scriptfrag.toggle();
-	}
+	})).marginRight(8);
+
+	console.label(prov(() => "Console")).marginLeft(8);
 });
 
 ui.onLoad(() => {
