7f30619b283351dc0c4986eaa6f821f87512c93c
DeltaNedas <deltanedas@gmail.com>
2020-11-18 21:23:07 +0000

    egg

--- scripts/button.js
+++ scripts/button.js
@@ -8,11 +8,11 @@
 
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with this program.	If not, see <https://www.gnu.org/licenses/>.
+	along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
 const ui = require("ui-lib/library");
@@ -32,9 +32,20 @@ ui.addButton("console", "terminal", toggle);
 /* or if you don't want to start a game */
 ui.addMenuButton("$console", "terminal", toggle);
 
-ui.addTable("bottom", "console", console => {
+// Always show console - even outside of a game
+ui.addArea("console", {
+	init(console) {
+		Core.scene.add(console.bottom().left());
+		console.visibility = () => shown;
+	},
+	post() {},
+
+	group: null
+});
+
+ui.addTable("console", "console", console => {
+	console.bottom().left();
 	console.background(Tex.buttonTrans);
-	console.visibility = () => shown;
 	console.defaults().margin(8);
 
 	// Clear the console
@@ -47,7 +58,7 @@ ui.addTable("bottom", "console", console => {
 		Vars.ui.scriptfrag.toggle();
 	}).marginRight(8);
 
-	console.add("Console").marginLeft(8);
+	console.add("$console").marginLeft(8);
 });
 
 ui.onLoad(() => {
